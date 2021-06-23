c93c843cd157184f8a5a16b6490f83c3e4ad2db6
Shubham27874 <60144043+Shubham27874@users.noreply.github.com>
2021-06-23 23:42:42 +0530

    update

--- Clone/script.js
+++ Clone/script.js
@@ -213,3 +213,15 @@ $(".icon-align-right").click(function() {
         updateCell("text-align","right",true);
     }
 });
+
+$(".color-fill-icon").click(function() {
+    $(".background-color-picker").click();
+})
+
+$(".color-fill-text").click(function() {
+    $(".text-color-picker").click();
+})
+
+$(".background-color-picker").change(function() {
+    updateCell("background-color", $(this).val())
+})
